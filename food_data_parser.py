795ad0a0f827f5f26bf01b866796150ec4748e24
Zack Kollar <rallokkcaz@gmail.com>
2017-09-03 09:55:02 -0700

    Added py.test tests, moved to class implementation.

--- food_data_parser.py
+++ food_data_parser.py
@@ -1,82 +1,81 @@
 import re
 import xlrd
-import glob
 
-from pprint import pprint
+class FoodDataParser:
 
-def addendum_regex():
-    addendum_pattern = r'^([0-9]+)(.+)'
-    return re.compile(addendum_pattern)
+    @property
+    def addendum_regex(self):
+        addendum_pattern = r'^([0-9]+)(.+)'
+        return re.compile(addendum_pattern)
 
-def get_valid_cell_rows(data, cell_cursor=3):
-    while not addendum_regex().match(data.cell_value(rowx=cell_cursor, colx=0)): # "USDA" not in data.cell_value(rowx=cell_cursor, colx=0):
-        if cell_cursor > data.nrows: break
-        yield (cell_cursor, data.cell_value(rowx=cell_cursor, colx=0))
-        cell_cursor += 1
+    def get_valid_cell_rows(self, cell_cursor=3):
+        while not self.addendum_regex.match(self.data.cell_value(rowx=cell_cursor, colx=0)):
+            if cell_cursor > self.data.nrows: break
+            yield (cell_cursor, self.data.cell_value(rowx=cell_cursor, colx=0))
+            cell_cursor += 1
 
-def get_addendum_by_id(data, addendum_id):
-    addendum_start = None
-    for row, _ in get_valid_cell_rows(data): addendum_start = row
+    def get_addendum_by_id(self, addendum_id):
+        addendum_start = None
+        for row, _ in self.get_valid_cell_rows(): addendum_start = row
 
-    addendum = addendum_regex()
+        addendum = self.addendum_regex
+        cell_cursor = addendum_start + 1
+        while cell_cursor < self.data.nrows:
+            cell_data = self.data.cell_value(rowx=cell_cursor, colx=0)
+            match = addendum.match(cell_data)
+            if match:
+                if int(match.groups()[0]) == addendum_id:
+                    return match.groups()[1]
 
-    cell_cursor = addendum_start + 1
-    while cell_cursor < data.nrows:
-        cell_data = data.cell_value(rowx=cell_cursor, colx=0)
-        match = addendum.match(cell_data)
-        if match:
-            if int(match.groups()[0]) == addendum_id:
-                return match.groups()[1]
-
-        cell_cursor += 1
+            cell_cursor += 1
 
 
-def get_data_columns(data, row):
-    row = data.row(row)[1:]
+    def get_data_columns(self, row):
+        row = self.data.row(row)[1:]
 
-    data = dict()
-    data['avg_retail_price'] = (row[0].value, row[1].value.strip())
-    data['prep_yield_factor'] = row[2].value
-    data['size_of_cup_eq'] = (row[3].value, row[4].value)
-    data['avg_price_per_cup'] = row[5].value
+        data = dict()
+        data['avg_retail_price'] = (row[0].value, row[1].value.strip())
+        data['prep_yield_factor'] = row[2].value
+        data['size_of_cup_eq'] = (row[3].value, row[4].value)
+        data['avg_price_per_cup'] = row[5].value
 
-    return data
+        return data
 
-def get_food_variants(data):
-    sub_column_name = None
-    for row, first_column in get_valid_cell_rows(data):
-        cell_pattern = r'([\w ]+)([0-9]+)$'
-        column_data = re.compile(cell_pattern).match(first_column)
+    def get_food_variants(self):
+        sub_column_name = None
+        for row, first_column in self.get_valid_cell_rows():
+            cell_pattern = r'([\w ]+)([0-9]+)$'
+            column_data = re.compile(cell_pattern).match(first_column.strip())
+            
+            if column_data is None and not self.data.cell_value(rowx=row, colx=1) and row > 0:
+                sub_column_name = first_column
+                continue
+            elif column_data is None:
+                print('FUCKITY SHIT')
+                continue
 
-        if column_data is None and not data.cell_value(rowx=row, colx=1):
-            sub_column_name = first_column
-            continue
-        elif column_data is None:
-            continue
+            column_info = column_data.groups()
+            column_data = self.get_data_columns(row)
+            column_data['storage_type'] = column_info[0] if not sub_column_name else f'{sub_column_name}: {column_info[0]}'
 
-        column_info = column_data.groups()
-        column_data = get_data_columns(data, row)
-        column_data['storage_type'] = column_info[0] if not sub_column_name else f'{sub_column_name}: {column_info[0]}'
+            addendum_id = int(column_info[1])
+            column_data['addendum'] = self.get_addendum_by_id(addendum_id)
 
-        addendum_id = int(column_info[1])
-        column_data['addendum'] = get_addendum_by_id(data, addendum_id)
+            yield column_data
 
-        yield column_data
+    def get_sources(self):
+        for row in self.data.col(0):
+            sources = re.compile(r'Source: (.+)').match(row.value)
+            if sources:
+                return sources.groups()[0]
 
-def get_food_data(xlsx_file):
-    data = xlrd.open_workbook(xlsx_file)
-    data = data.sheet_by_index(0)
+    def parse(self, xlsx_file):
+        self.data = xlrd.open_workbook(xlsx_file)
+        self.data = self.data.sheet_by_index(0)
 
-    food_data = dict()
-    food_data['name'] = data.name
-    food_data['variants'] = list(get_food_variants(data))
+        food_data = dict()
+        food_data['name'] = self.data.name
+        food_data['variants'] = list(self.get_food_variants())
+        food_data['sources'] = self.get_sources()
 
-    for row in data.col(0):
-        sources = re.compile(r'Source: (.+)').match(row.value)
-        if sources:
-            food_data['sources'] = sources.groups()[0]
-
-    return food_data
-
-for filename in glob.glob('/home/breath/Downloads/food_data/*.xlsx'):
-    pprint(get_food_data(filename))
+        return food_data
---
+++ food_data_parser_test.py
@@ -0,0 +1,30 @@
+import os
+import glob
+import unittest
+import pprint
+
+from food_data_parser import FoodDataParser
+
+
+FOOD_DATA_PATH = '/home/breath/Downloads/food_data/'
+FOOD_DATA_GLOB = os.path.join(FOOD_DATA_PATH, '*.xlsx')
+
+class TestFoodDataParser(unittest.TestCase):
+
+    def setUp(self):
+        self.food_data_files = glob.glob(FOOD_DATA_GLOB)
+        self.foo_data = self.food_data_files[0]
+
+        self.parser = FoodDataParser()
+        self.data = self.parser.parse(self.foo_data)
+
+    def test_parses_all_data(self):
+        for filename in self.food_data_files:
+
+            data = self.parser.parse(filename)
+            variants_count = len(data['variants'])
+
+            self.assertTrue(variants_count > 0)
+
+    def test_creates_proper_data_structure(self):
+        self.assertTrue({'name', 'variants', 'sources'} == self.data.keys())
